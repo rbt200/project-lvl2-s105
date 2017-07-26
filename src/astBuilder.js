d81e418e2780fe76211c5fd3b116cb0413c6a62b
myname <myesoap@mail.ru>
2017-07-26 13:17:44 +0300

    add astBuilder astRender

--- src/astBuilder.js
+++ src/astBuilder.js
@@ -2,43 +2,39 @@ import _ from 'lodash';
 
 /*
 {
-  key: 'key'
-  value: 'value'
-  status: 'unchanged' / 'added' / 'removed' / 'updated'
-  update: 'value'
+  type: 'unchanged' / 'changed' / 'added' / 'removed',
+  key: 'key',
+  valueOld: 'value',
+  valueNew: 'value',
 }
 */
+
+const getType = (val1, val2) => {
+  if (val1 === val2) {
+    return 'unchanged';
+  }
+  if (!val1) {
+    return 'added';
+  }
+  if (!val2) {
+    return 'removed';
+  }
+  return 'changed';
+};
 
 const astBuilder = (obj1, obj2) => {
   const unitedKeys = _.union(Object.keys(obj1), Object.keys(obj2));
 
   const result = unitedKeys.map((key) => {
-    const value1 = obj1[key];
-    const value2 = obj2[key];
-    const node = {};
-    node.key = key;
-    if (!_.has(obj1, key)) {
-      node.value = value2;
-      node.status = 'added';
-      return node;
-    }
-    if (!_.has(obj2, key)) {
-      node.value = value1;
-      node.status = 'removed';
-      return node;
-    }
-    if (_.has(obj1, key) === _.has(obj2, key)) {
-      if (value1 === value2) {
-        node.value = value1;
-        node.status = 'unchanged';
-        return node;
-      }
-      node.value = value2;
-      node.status = 'updated';
-      node.update = value1;
-      return node;
-    }
-    return node;
+    const valueOld = obj1[key];
+    const valueNew = obj2[key];
+    const type = getType(valueOld, valueNew);
+    return {
+      type,
+      key,
+      valueOld,
+      valueNew,
+    };
   });
   return result;
 };
--- src/astRender.js
+++ src/astRender.js
@@ -1,14 +1,27 @@
-import convertStatus from './status';
+const newLine = '\n';
 
-const NewLine = '\n';
+const convertType = (data) => {
+  switch (data) {
+    case 'changed':
+      return '  + ';
+    case 'added':
+      return '  + ';
+    case 'removed':
+      return '  - ';
+    default:
+      return '    ';
+  }
+};
 
 const stringBuilder = (obj) => {
-  const str = `${NewLine}${convertStatus(obj.status)}${obj.key}: ${obj.value}`;
-  if (obj.update) { return str.concat(`${NewLine}${convertStatus('removed')}${obj.key}: ${obj.update}`); }
+  const str = `${newLine}${convertType(obj.type)}${obj.key}: ${obj.valueNew !== undefined ? obj.valueNew : obj.valueOld}`;
+  if (obj.type === 'changed') {
+    return str.concat(`${newLine}${convertType('removed')}${obj.key}: ${obj.valueOld}`);
+  }
   return str;
 };
 
 export default (ast) => {
   const res = ast.reduce((acc, item) => acc.concat(stringBuilder(item)), '');
-  return `{${res}${NewLine}}`;
+  return `{${res}${newLine}}`;
 };
