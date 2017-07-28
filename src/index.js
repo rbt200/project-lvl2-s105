047197d9dcc21ece96344180e67b3fea2088560e
myname <myesoap@mail.ru>
2017-07-28 11:46:02 +0300

    unite two astRenders into one

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import fs from 'fs';
 import path from 'path';
 import getParser from './parsers';
-import getRender from './renders';
 import astBuilder from './astBuilder';
+import getRender from './astUniRender';
 
 export default (file1, file2) => {
   const ext1 = path.parse(file1).ext;
   const ext2 = path.parse(file2).ext;
   const obj1 = getParser(ext1)(fs.readFileSync(file1, 'utf-8'));
   const obj2 = getParser(ext2)(fs.readFileSync(file2, 'utf-8'));
-  return getRender(obj1)(astBuilder(obj1, obj2));
+  return getRender(astBuilder(obj1, obj2));
 };
