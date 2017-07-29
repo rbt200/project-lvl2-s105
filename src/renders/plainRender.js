7057e0bfd5e71f49c539738ccda2a7ccdaad095a
myname <myesoap@mail.ru>
2017-07-29 18:46:06 +0300

    refactor plainRender

--- src/renders/plainRender.js
+++ src/renders/plainRender.js
@@ -4,19 +4,20 @@ const newLine = '\n';
 
 const stringBuilder = (obj, ...parent) =>
   _.reduce(obj, (acc, item) => {
-    const transfer = `${item.key}${parent === '' ? '' : '.'}${parent}`;
-    if (item.children) {
-      return acc.concat(stringBuilder(item.children, `${transfer}`));
+    const path = `${item.key}${parent === '' ? '' : '.'}${parent}`;
+    const type = item.type;
+    if (type === 'nested') {
+      return acc.concat(stringBuilder(item.children, `${path}`));
     }
-    if (item.type === 'changed') {
+    if (type === 'changed') {
       return `${acc}${newLine}Property '${parent}${item.key}' was updated. From ` +
       `'${item.valueOld}' to '${item.valueNew}'`;
     }
-    if (item.type === 'added') {
+    if (type === 'added') {
       const value = _.isObject(item.valueNew) ? 'complex value' : `value: ${item.valueNew}`;
       return `${acc}${newLine}Property '${parent}${item.key}' was added with ${value}`;
     }
-    if (item.type === 'removed') {
+    if (type === 'removed') {
       return `${acc}${newLine}Property '${parent}${item.key}' was removed`;
     }
     return acc;
