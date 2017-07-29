45fa7e32307a70ded3cb65dbed5d351bb0676f8c
myname <myesoap@mail.ru>
2017-07-29 18:02:37 +0300

    change name getRender into astRender

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import fs from 'fs';
 import path from 'path';
 import getParser from './parsers';
 import astBuilder from './astBuilder';
-import getRender from './renders';
+import astRender from './renders';
 
 export default (file1, file2, format = 'structured') => {
   const ext1 = path.extname(file1);
   const ext2 = path.extname(file2);
   const obj1 = getParser(ext1)(fs.readFileSync(file1, 'utf-8'));
   const obj2 = getParser(ext2)(fs.readFileSync(file2, 'utf-8'));
-  return getRender(format, astBuilder(obj1, obj2));
+  return astRender(format, astBuilder(obj1, obj2));
 };
