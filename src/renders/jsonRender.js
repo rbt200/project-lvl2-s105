740730744335d9068e22cbf4d3896b9d75297678
myname <myesoap@mail.ru>
2017-08-01 10:19:06 +0300

    refactor jsonRender linting error

--- src/renders/jsonRender.js
+++ src/renders/jsonRender.js
@@ -1,4 +1,4 @@
 import _ from 'lodash';
 
-export default ast => JSON.stringify(ast, (key, value) => _.isNumber(value) ? value.toString() : value, '  ');
+export default ast => JSON.stringify(ast, (key, value) => (_.isNumber(value) ? value.toString() : value), '  ');
 
