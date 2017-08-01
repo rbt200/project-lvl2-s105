671ad0468b79a6d80033a350ed55fb7ac3477a18
myname <myesoap@mail.ru>
2017-08-01 09:24:49 +0300

    refactor task5 task6

--- __tests__/test.js
+++ __tests__/test.js
@@ -43,15 +43,6 @@ describe('gendiff tests', () => {
 Property 'proxy' was removed
 Property 'verbose' was added with value: true`;
 
-  const result4 =
-`Property 'common.setting2' was removed
-Property 'common.setting6' was removed
-Property 'common.setting4' was added with value: blah blah
-Property 'common.setting5' was added with complex value
-Property 'group1.baz' was updated. From 'bas' to 'bars'
-Property 'group2' was removed
-Property 'group3' was added with complex value`;
-
   const result5 =
   `[
   {
@@ -164,6 +155,47 @@ Property 'group3' was added with complex value`;
   }
 ]`;
 
+  const result6 = `{
+    common: {
+        setting1: Value 1
+      - setting2: 200
+        setting3: true
+      - setting6: {
+            key: value
+        }
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+            key6: value6
+            key7: value7
+            settin10: {
+                item1: name1
+                item2: name2
+            }
+        }
+    }
+    group1: {
+      + baz: bars
+      - baz: bas
+        foo: bar
+    }
+  - group2: {
+        abc: 12345
+    }
+  + group3: {
+        fee: 100500
+    }
+}`;
+
+  const result7 =
+`Property 'common.setting2' was removed
+Property 'common.setting6' was removed
+Property 'common.setting4' was added with value: blah blah
+Property 'common.setting5' was added with complex value
+Property 'group1.DDD.KKK.goo' was added with value: gl
+Property 'group2' was removed
+Property 'group3' was added with complex value`;
+
   it('Compare two .json files', () => {
     const before = generatePath('before', 'json');
     const after = generatePath('after', 'json');
@@ -218,24 +250,6 @@ Property 'group3' was added with complex value`;
     expect(gendiff(before, after, 'plain')).toBe(result3);
   });
 
-  it('Compare two nested .json files with plain format', () => {
-    const before = generatePath('before', 'json', true);
-    const after = generatePath('after', 'json', true);
-    expect(gendiff(before, after, 'plain')).toBe(result4);
-  });
-
-  it('Compare two nested .yml files  with plain format', () => {
-    const before = generatePath('before', 'yml', true);
-    const after = generatePath('after', 'yml', true);
-    expect(gendiff(before, after, 'plain')).toBe(result4);
-  });
-
-  it('Compare two nested .ini files  with plain format', () => {
-    const before = generatePath('before', 'ini', true);
-    const after = generatePath('after', 'ini', true);
-    expect(gendiff(before, after, 'plain')).toBe(result4);
-  });
-
   it('Compare two nested .json files with json format', () => {
     const before = generatePath('before', 'json', true);
     const after = generatePath('after', 'json', true);
@@ -252,5 +266,17 @@ Property 'group3' was added with complex value`;
     const before = generatePath('before', 'yml', true);
     const after = generatePath('after', 'yml', true);
     expect(gendiff(before, after, 'json')).toBe(result5);
+  });
+
+  it('Compare two nested deep structured .json files ', () => {
+    const before = './__tests__/fixtures/deepnestedBefore.json';
+    const after = './__tests__/fixtures/deepnestedAfter.json';
+    expect(gendiff(before, after)).toBe(result6);
+  });
+
+  it('Compare two nested .json files with plain format', () => {
+    const before = './__tests__/fixtures/plainBefore.json';
+    const after = './__tests__/fixtures/plainAfter.json';
+    expect(gendiff(before, after, 'plain')).toBe(result7);
   });
 });
--- src/renders/structuredRender.js
+++ src/renders/structuredRender.js
@@ -5,31 +5,32 @@ const insertWSpace = (quantity = 0) => _.repeat('    ', quantity);
 
 const stringBuilder = (object, shift) => {
   if (_.isObject(object)) {
-    const keys = Object.keys(object);
-    return `{${keys.reduce((acc, key) => { if (key){}return `${acc}${newLine}${insertWSpace(shift + 2)}${key}: ` +
-    `${object[key]}` ;}, '')}${newLine}${insertWSpace(shift  + 1)}}`;
+    return `{${Object.keys(object).reduce((acc, key) => {
+      const value = object[key];
+      if (_.isObject(value)) {
+        return acc.concat(`${newLine}${insertWSpace(shift + 2)}${key}: ${stringBuilder(value, shift + 1)}`);
+      }
+      return `${acc}${newLine}${insertWSpace(shift + 2)}${key}: ${value}`;
+    }, '')}${newLine}${insertWSpace(shift + 1)}}`;
   }
   return object;
 };
 
-const getAdded = (type, item, shift) => {
-  return `${newLine}${insertWSpace(shift)}${'  + '}${item.key}: ${stringBuilder(item.valueNew, shift)}`;
-};
-const getRemoved = (type, item, shift) => {
-  return `${newLine}${insertWSpace(shift)}${'  - '}${item.key}: ${stringBuilder(item.valueOld, shift)}`;
-};
-const getUnchanged = (type, item, shift) => {
-  return `${newLine}${insertWSpace(shift)}${'    '}${item.key}: ${item.valueOld}`;
-};
-const getChanged = (type, item, shift) => {
-  return `${newLine}${insertWSpace(shift)}${'  + '}${item.key}: ${stringBuilder(item.valueNew, shift)}` +
-        `${newLine}${insertWSpace(shift)}${'  - '}${item.key}: ${stringBuilder(item.valueOld, shift)}`;
-};
-const getNested = (type, item, shift, render) => {
-  return `${newLine}${insertWSpace(shift)}${'    '}${item.key}: {` +
-        `${render}${newLine}${insertWSpace(shift + 1)}}`;
-};
+const getAdded = (type, item, shift) => `${newLine}${insertWSpace(shift)}${'  + '}` +
+  `${item.key}: ${stringBuilder(item.valueNew, shift)}`;
 
+const getRemoved = (type, item, shift) => `${newLine}${insertWSpace(shift)}${'  - '}` +
+  `${item.key}: ${stringBuilder(item.valueOld, shift)}`;
+
+const getUnchanged = (type, item, shift) => `${newLine}${insertWSpace(shift)}` +
+  `${'    '}${item.key}: ${item.valueOld}`;
+
+const getChanged = (type, item, shift) => `${newLine}${insertWSpace(shift)}${'  + '}` +
+  `${item.key}: ${stringBuilder(item.valueNew, shift)}` +
+    `${newLine}${insertWSpace(shift)}${'  - '}${item.key}: ${stringBuilder(item.valueOld, shift)}`;
+
+const getNested = (type, item, shift, render) => `${newLine}${insertWSpace(shift)}${'    '}` +
+  `${item.key}: {${render}${newLine}${insertWSpace(shift + 1)}}`;
 
 const render = (ast, shift = 0) =>
   ast.reduce((acc, item) => {
